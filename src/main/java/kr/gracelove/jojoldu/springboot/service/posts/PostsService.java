8307f4590f2161b668883532d58d6f93446e5502
gracelove <govlmo91@gmail.com>
2019-12-06 03:53:45 +0900

    put method , test

--- src/main/java/kr/gracelove/jojoldu/springboot/service/posts/PostsService.java
+++ src/main/java/kr/gracelove/jojoldu/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package kr.gracelove.jojoldu.springboot.service.posts;
 
+import kr.gracelove.jojoldu.springboot.domain.posts.Posts;
 import kr.gracelove.jojoldu.springboot.domain.posts.PostsRepository;
+import kr.gracelove.jojoldu.springboot.web.dto.PostsResponseDto;
 import kr.gracelove.jojoldu.springboot.web.dto.PostsSaveRequestDto;
+import kr.gracelove.jojoldu.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,17 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow( () -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow( () -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+        return new PostsResponseDto(entity);
     }
 }
--- src/test/java/kr/gracelove/jojoldu/springboot/web/PostsApiControllerTest.java
+++ src/test/java/kr/gracelove/jojoldu/springboot/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package kr.gracelove.jojoldu.springboot.web;
 import kr.gracelove.jojoldu.springboot.domain.posts.Posts;
 import kr.gracelove.jojoldu.springboot.domain.posts.PostsRepository;
 import kr.gracelove.jojoldu.springboot.web.dto.PostsSaveRequestDto;
+import kr.gracelove.jojoldu.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.After;
@@ -12,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -63,6 +66,41 @@ public class PostsApiControllerTest {
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
+     }
+
+     @Test
+     public void post수정된다() throws Exception {
+         //given
+         Posts savePosts = postsRepository.save(Posts.builder()
+                 .title("title")
+                 .content("content")
+                 .author("author")
+                 .build());
+
+         Long updateId = savePosts.getId();
+         String expectedTitle = "updatedTitle";
+         String expectedContent = "updatedContent";
+
+         PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                 .title(expectedTitle)
+                 .content(expectedContent)
+                 .build();
+
+         String url = "http://localhost:"+port+"/api/v1/posts/"+updateId;
+
+         HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+         //when
+         ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+         //then
+         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+         assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+         List<Posts> all = postsRepository.findAll();
+         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+         assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+
      }
 
 }
