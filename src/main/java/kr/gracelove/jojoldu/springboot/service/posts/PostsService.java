da3962fa017712b628971e795d37dd7b041e5c4d
gracelove <govlmo91@gmail.com>
2019-12-06 11:38:45 +0900

    글 전체목록 출력

--- src/main/java/kr/gracelove/jojoldu/springboot/service/posts/PostsService.java
+++ src/main/java/kr/gracelove/jojoldu/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package kr.gracelove.jojoldu.springboot.service.posts;
 
 import kr.gracelove.jojoldu.springboot.domain.posts.Posts;
 import kr.gracelove.jojoldu.springboot.domain.posts.PostsRepository;
+import kr.gracelove.jojoldu.springboot.web.dto.PostsListResponseDto;
 import kr.gracelove.jojoldu.springboot.web.dto.PostsResponseDto;
 import kr.gracelove.jojoldu.springboot.web.dto.PostsSaveRequestDto;
 import kr.gracelove.jojoldu.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,5 +33,12 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow( () -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
