b7957bde3a744e17d7d76775b9f2e00bbfef213a
gracelove <govlmo91@gmail.com>
2019-12-06 12:42:06 +0900

    게시글 업데이트까지

--- build/resources/main/static/js/app/index.js
+++ build/resources/main/static/js/app/index.js
@@ -1,11 +1,13 @@
-const index = {
+const main = {
     init: function () {
         const _this = this;
         $('#btn-save').on('click', function () {
             _this.save();
+        });
+        $('#btn-update').on('click', function () {
+            _this.update();
         })
     },
-
     save : function () {
         const data = {
             title : $('#title').val(),
@@ -25,7 +27,28 @@ const index = {
         }).fail(function (err) {
             alert(JSON.stringify(err));
         });
+    },
+    update : function() {
+        const data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        const id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json;charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function(err) {
+            alert(JSON.stringify(err));
+        });
     }
 };
 
-index.init();
+main.init();
