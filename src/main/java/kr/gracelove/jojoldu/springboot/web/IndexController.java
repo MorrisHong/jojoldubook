31e1276df0b1d3d5af4309035c917441daccf680
gracelove <govlmo91@gmail.com>
2019-12-07 00:42:07 +0900

    구글 로그인 구현완료

--- src/main/java/kr/gracelove/jojoldu/springboot/web/IndexController.java
+++ src/main/java/kr/gracelove/jojoldu/springboot/web/IndexController.java
@@ -22,7 +22,7 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
-        User user = (User) httpSession.getAttribute("user");
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
