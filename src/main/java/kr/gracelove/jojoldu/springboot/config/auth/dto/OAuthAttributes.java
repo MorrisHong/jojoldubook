b9a1c356ccdda9a698f82524758fb533a3885a24
gracelove <govlmo91@gmail.com>
2019-12-07 13:58:13 +0900

    naver login

--- src/main/java/kr/gracelove/jojoldu/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/kr/gracelove/jojoldu/springboot/config/auth/dto/OAuthAttributes.java
@@ -28,6 +28,10 @@ public class OAuthAttributes {
                                      String userNameAttributeName,
                                      Map<String, Object> attributes) {
 
+        if("naver".equals(registrationId)) {
+            return ofNaver("id",attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -37,6 +41,18 @@ public class OAuthAttributes {
                 .email(String.valueOf(attributes.get("email")))
                 .picture(String.valueOf(attributes.get("picture")))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profileImage"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
