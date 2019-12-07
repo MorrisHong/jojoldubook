2d60d6e3f6ba347cfb8f55bbdabba0b3826f95f9
gracelove <govlmo91@gmail.com>
2019-12-07 13:30:27 +0900

    @LoginUser 적용

--- src/main/java/kr/gracelove/jojoldu/springboot/web/IndexController.java
+++ src/main/java/kr/gracelove/jojoldu/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package kr.gracelove.jojoldu.springboot.web;
 
+import kr.gracelove.jojoldu.springboot.config.auth.LoginUser;
 import kr.gracelove.jojoldu.springboot.config.auth.dto.SessionUser;
 import kr.gracelove.jojoldu.springboot.domain.user.User;
 import kr.gracelove.jojoldu.springboot.service.posts.PostsService;
@@ -17,12 +18,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
